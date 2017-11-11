78075a044ac6d1be9845561abfc20ceadfcf6398
R. "Patches" S <patches@nullcorp.org>
2017-11-11 12:22:00 -0500

    Cleanup of old ways, added some formatting, satisfied PEP8

--- tableAudio.py
+++ tableAudio.py
@@ -1,22 +1,21 @@
-from html.parser import HTMLParser, HTMLParseError
-import urllib.request
-import subprocess
+from html.parser import HTMLParser
+import requests
+import shutil
+import os
 import re
 import concurrent.futures
 
 
 # URL we fetch.
 urlTA = 'http://tabletopaudio.com/'
-localFile = '/databank/Audio/Music/Ambience/'
+localFile = '/media/databank/Audio/Music/Ambience/'
 
 # We need dis.
 endList = []
 
 # The 'ls' of the directory containing the files
-listResult = subprocess.check_output("ls {}".format(localFile),
-                                     universal_newlines = True,
-                                     stderr = subprocess.STDOUT,
-                                     shell = True)
+listResult = os.listdir(localFile)
+
 
 # Our custom parser to build the list we will need.
 class MyHTMLParser(HTMLParser):
@@ -36,19 +35,32 @@ class MyHTMLParser(HTMLParser):
                         cleanURL = urlTA + cleanAttr
 
                         # Build list containing important information.
-                        endList.append({"url": cleanURL, "file": localFile + match.group(0)})
+                        endList.append(
+                            {
+                                "url": cleanURL,
+                                "file": localFile + match.group(0)
+                            }
+                        )
+
 
 # Function for our concurrent processes.
 def downloadURL(url, local):
-    print("GET: ", url)
-    urllib.request.urlretrieve(url, local)
+    req = requests.get(url, stream=True, headers={'User-Agent': 'Mozilla/5.0'})
+    if req.status_code == 200:
+        print("GET: ", url)
+        with open(local, 'wb') as f:
+            req.raw.decode_content = True
+            shutil.copyfileobj(req.raw, f)
+
 
 # Do the parsing work.
 parser = MyHTMLParser()
-tableAudio = urllib.request.urlopen(urlTA)
+# req = urllib.request.Request(urlTA, headers={'User-Agent': 'Mozilla/5.0'})
+# tableAudio = urllib.request.urlopen(req)
+tableAudio = requests.get(urlTA, headers={'User-Agent': 'Mozilla/5.0'})
 try:
-    parser.feed(str(tableAudio.read()))
-except (TypeError, HTMLParseError):
+    parser.feed(tableAudio.text)
+except Exception:  # HTMLParseError removed?
     # We pass because it just happens when we run out of shit.
     # Also, the site uses malformed html. So we have to just...accept that.
     pass
@@ -56,7 +68,9 @@ except (TypeError, HTMLParseError):
 # Ripped from concurrent manpage.
 with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
     # Start the load operations and mark each future with its URL
-    future_to_url = {executor.submit(downloadURL, x["url"], x["file"]): x for x in endList}
+    future_to_url = {
+        executor.submit(downloadURL, x["url"], x["file"]): x for x in endList
+    }
     for future in concurrent.futures.as_completed(future_to_url):
         url = future_to_url[future]
         try:
